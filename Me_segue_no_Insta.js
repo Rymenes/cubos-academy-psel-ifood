1aa286d80d8ecb1df9fe3852eea1dd61f94f7639
Rymenes <rymenes@gmail.com>
2021-06-16 01:02:17 -0300

    Capricha na foto para ganhar vários seguidores.

---
+++ Me_segue_no_Insta.js
@@ -0,0 +1,11 @@
+function solucao(largura, altura) {
+  //seu codigo aqui
+    if(largura < altura){
+        console.log("RETRATO");
+    }else if (largura > altura){
+        console.log("PAISAGEM");
+    }else {console.log("QUADRADA");
+          }
+    
+  
+}
