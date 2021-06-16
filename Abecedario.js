469926fc699819992c1780358e19f23bb70b82eb
Rymenes <rymenes@gmail.com>
2021-06-16 01:34:20 -0300

    Já ouviu falar da bincadeira de criança chamada Adedonha ?

---
+++ Abecedario.js
@@ -0,0 +1,10 @@
+function solucao(letra, palavras) {
+	let resultado = 0;
+    for (let item of palavras){
+        if (item [0] != letra){
+            resultado ++;
+            
+        }
+    }console.log (resultado)
+    
+}
